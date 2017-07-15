e3b4d9207b7e752f764b436eabe7eabcb4bd5904
Lyuben Petrov <lyuben.y.petrov@gmail.com>
2017-07-15 11:22:53 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'cinch'
+gem 'config'
+gem 'feedjira'
+gem 'httparty'
---
+++ photo.rb
@@ -0,0 +1,80 @@
+require 'cinch'
+require 'feedjira'
+require 'httparty'
+require 'config'
+
+Config.load_and_set_settings(File.join(File.dirname(__FILE__),
+                                       'settings.local.yml'))
+
+class PhotoPlugin
+  include Cinch::Plugin
+  attr_accessor :already_posted
+
+  timer 20, method: :check_and_post
+
+  def initialize(*args)
+    super
+
+    @already_posted = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def check_and_post
+    Settings.rss_urls.each do |url|
+      post_entries_for(url)
+    end
+  end
+
+  def post_entries_for(url)
+    get_entries_for(url).each do |entry|
+      already_posted[url] << entry.published
+      Channel(Settings.channel).notice(format_entry(entry))
+    end
+  end
+
+  def format_entry(entry)
+    "#{entry.title}   #{entry.url.gsub(/&?\?*utm_.+?(&|$)/, '')}"
+  end
+
+  def get_entries_for(url)
+    tries = 0
+
+    begin
+      xml = HTTParty.get(url).body
+      p "Fetching #{url}"
+      p "got a #{xml.length} long response"
+      feed = Feedjira::Feed.parse(xml)
+      entries =
+        feed.entries
+        .select { |x| x.published > Time.now - 12 * 60 * 60 }
+        .reject { |x| already_posted[url].include?(x.published) }
+    rescue Feedjira::NoParserAvailable
+      p "Refetching for #{url}"
+      tries += 1
+      if tries > 3
+        p "Failed to fetch #{url} :("
+        entries = []
+      else
+        sleep(20)
+        retry
+      end
+    end
+
+    p "Fetched #{url} at #{Time.now} with #{entries.length} entries"
+    entries
+  end
+end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.user = Settings.user
+    c.nick = Settings.nick
+    c.server = Settings.server
+    c.ssl.use = Settings.use_ssl
+    c.port = Settings.port
+    c.channels = [Settings.channel]
+    c.verbose = true
+    c.plugins.plugins = [PhotoPlugin]
+  end
+end
+
+bot.start
